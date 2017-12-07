e8b972b0e0dbcb464b9c2d1115ada64d06b85b77
officel <nishimuray@gmail.com>
2017-12-07 20:36:53 +0900

    add monitor cpu

---
+++ monitor/monitor_cpu.tf
@@ -0,0 +1,45 @@
+# DataDog Monitor
+
+resource "datadog_monitor" "prod_web_cpu" {
+  // Title
+  name = "[terraform] [${var.project}] [prod-web] [system.cpu.system]"
+
+  // Choose the detection method
+  type = "query alert"
+
+  // Define the metric
+  query = "avg(last_5m):avg:system.cpu.system{role-prod-web,project:${var.project}} by {host} > 8"
+
+  // Set alert conditions
+  thresholds {
+    critical = "8"
+    warning  = "6"
+    ok       = "5"
+  }
+  require_full_window = false
+  notify_no_data = false
+  no_data_timeframe = 10
+  timeout_h = 1
+  new_host_delay = "300"
+  evaluation_delay = 0
+  
+  // Say what's happening
+  message = <<EOF
+  CPU使用率が高いホスト {host.name} ({host.ip})
+  ${var.notify}
+  EOF
+
+  renotify_interval = "60"
+
+  escalation_message = <<EOF
+  High CPU usage for last 5m on host {host.name} ({host.ip}) hasn't been solved ${var.escalation_notify}
+  EOF
+
+  // Notify your team
+  notify_audit = false
+  locked = true
+
+  // other
+  tags = ["${var.project}"]
+}
+
